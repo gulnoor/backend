a0f62d0a5c908a7dfa3d2553ecc06f915fb35416
gulnoor <gulnoor5233@gmail.com>
2026-08-20 04:32:55 +0500

    uibuild

--- dist/backend/index.js
+++ dist/backend/index.js
@@ -1,15 +1,13 @@
 import express from "express";
-
 const app = express();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist/public"));
+    app.use(express.static("dist/public"));
 }
-
 app.get("/api/ping", (req, res) => {
-  return res.send("pong");
+    return res.send("pong");
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`server running on port:${PORT}`);
+    console.log(`server running on port:${PORT}`);
 });
+//# sourceMappingURL=index.js.map
