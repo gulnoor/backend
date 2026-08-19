a0ee880901a89fdd380e6c72742fd961f2e3e262
gulnoor <gulnoor5233@gmail.com>
2026-08-20 03:32:57 +0500

    uibuild

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+
 const app = express();
+app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
   res.send("pong");
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port:${PORT}`);
 });
